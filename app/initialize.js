023adace66d73bdde3987060681826ba1d25042e
Christopher Motl <cmotl@cmotl.com>
2018-04-22 17:03:08 -0500

    :new: Apply basic view model bindings
    Problem:
    - The main view interface has no bindings to underlying data.
    
    Solution:
    - Apply a basic view model to expand upon as the model grows.

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,11 @@
+var ko = require('knockout')
+
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
+	var view_model = {
+    title: ko.observable("Ham Test")
+	};
+	 
+	ko.applyBindings(view_model, document.getElementById("title"))
 });
