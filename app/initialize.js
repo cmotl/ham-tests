d53caeac61ad3de66ecbf0d9ef2840f251dd6869
Christopher Motl <cmotl@cmotl.com>
2018-04-22 19:01:52 -0500

    :new: Sample connection of model to interface
    Problem:
    - Questions can be filtered, but they are not tied to the external interface.
    
    Solution:
    - Connect a sample of some filtered questions to the external interface.

--- app/initialize.js
+++ app/initialize.js
@@ -1,11 +1,15 @@
 var ko = require('knockout')
+var questions = require('lib/questions')
 
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
 	var view_model = {
-    title: ko.observable("Ham Test")
+    title: ko.observable("Ham Test"),
+    questions: ko.observableArray()
 	};
 	 
-	ko.applyBindings(view_model, document.getElementById("title"))
+	ko.applyBindings(view_model, document.getElementById("content"))
+
+  view_model.questions(questions.for_sections(["4.3.1", "4.3.2"]))
 });
