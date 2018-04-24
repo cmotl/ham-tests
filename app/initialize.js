19c929036ab9738b2bf368efcd03c99dc335a559
Christopher Motl <cmotl@cmotl.com>
2018-04-23 21:05:39 -0500

    :question: Display questions and answers
    Problem:
    - The only information displayed are question ids.
    
    Solution:
    - Update interface template to display questions and answers.

--- app/initialize.js
+++ app/initialize.js
@@ -2,14 +2,12 @@ var ko = require('knockout')
 var questions = require('lib/questions')
 
 document.addEventListener('DOMContentLoaded', () => {
-  // do your setup here
   console.log('Initialized app');
 	var view_model = {
-    title: ko.observable("Ham Test"),
     questions: ko.observableArray()
 	};
 	 
 	ko.applyBindings(view_model, document.getElementById("content"))
 
-  view_model.questions(questions.for_sections(["4.3.1", "4.3.2"]))
+  view_model.questions(questions.for_sections(["4.1","4.2"]))
 });
