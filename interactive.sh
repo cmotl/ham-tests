d940fdd15e2cb9f1efc5b20fa74c3df5e644ae2c
Ben Goodwyn <bengoodwyn@me.com>
2018-04-24 08:25:32 -0500

    :unlock: Run as current user
    Problem:
    - Running `interactive.sh` creates root-owned folders and files on my
    filesystem
    
    Solution:
    - Start the docker container with the UID and GID of the current user so
    that the files will be correctly owned
    - Create a home directory for use in the docker environment
    
    Notes:
    - This may require first removing the root-owned folders prior to being
    able to run as a non-privileged user

--- interactive.sh
+++ interactive.sh
@@ -1,1 +1,3 @@
-docker run -it --rm -p 3333:3333 -v ${PWD}:/src -w /src node:9.11.1 bash
+#!/bin/bash
+mkdir -p .home
+docker run -it --rm -p 3333:3333 -u $(id -u):$(id -g) -e "HOME=/src/.home" -v ${PWD}:/src -w /src node:9.11.1 bash
