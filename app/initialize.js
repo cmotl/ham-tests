b41d28d7108fd1f088e87b5da3571cc5617acef4
Christopher Motl <cmotl@cmotl.com>
2018-04-23 23:00:30 -0500

    :memo: Show one question at a time
    Problem:
    - All questions and answers are displayed.
    
    Solution:
    - Display single question at a time with mechanism to navigate between questions.

--- app/initialize.js
+++ app/initialize.js
@@ -3,11 +3,31 @@ var questions = require('lib/questions')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Initialized app');
-	var view_model = {
+  var view_model = {
     questions: ko.observableArray(),
-	};
+    selectedIndex: ko.observable(),
+  };
 	 
-	ko.applyBindings(view_model, document.getElementById("content"))
+  view_model.selectedQuestion = ko.computed(() => {
+    return view_model.questions()[view_model.selectedIndex()]    
+  })
+  view_model.previous = () => {
+    view_model.selectedIndex(view_model.selectedIndex()-1)
+  }
+  view_model.next = () => {
+    view_model.selectedIndex(view_model.selectedIndex()+1)
+  }
+  view_model.hasPrevious = ko.computed(() => {
+    return view_model.selectedIndex() > 0
+  })
+  view_model.hasNext = ko.computed(() => {
+    return view_model.selectedIndex() < (view_model.questions().length - 1)     
+  })
 
-  view_model.questions(questions.for_sections(["4.1","4.2"]))
+  ko.applyBindings(view_model, document.getElementById("content"))
+
+  view_model.selectedQuestion.subscribe(console.log)
+
+  view_model.questions(questions.for_sections(["2.1.1"]))
+  view_model.selectedIndex(0)
 });
