f932bf2b89cd4f37ab10d3fb43acfd63c5a22db5
Christopher Motl <cmotl@cmotl.com>
2018-04-26 23:28:06 -0500

    :8ball: Reveal the correct answer
    Problem:
    - The correct answer is bolded as soon as the question is displayed.
    - Always showing the correct answer provides no opportunity for the user
      to perform a forced recall of the correct answer.
    
    Solution:
    - Hide the aswer upon display of a new question.
    - Reveal the answer when the question or answers are clicked on.

--- app/initialize.js
+++ app/initialize.js
@@ -6,23 +6,29 @@ document.addEventListener('DOMContentLoaded', () => {
     sections: ko.observableArray(),
     selectedSection: ko.observableArray(),
     questions: ko.observableArray(),
-    selectedIndex: ko.observable()
+    selectedIndex: ko.observable(),
+    show_answer: ko.observable(false),
   };
 
+  view_model.reveal_answer = () => { view_model.show_answer(true)}
+
   ko.computed(() => {
-    console.log("questions")
-    console.log(questions.for_sections(view_model.selectedSection()))
     view_model.questions(questions.for_sections(view_model.selectedSection()))
     view_model.selectedIndex(0)
+    view_model.show_answer(false)
   })
 
+  view_model.show_answer.subscribe(console.log)
   view_model.selectedQuestion = ko.computed(() => {
     return view_model.questions()[view_model.selectedIndex()]    
   })
+
   view_model.previous = () => {
+    view_model.show_answer(false)
     view_model.selectedIndex(view_model.selectedIndex()-1)
   }
   view_model.next = () => {
+    view_model.show_answer(false)
     view_model.selectedIndex(view_model.selectedIndex()+1)
   }
   view_model.hasPrevious = ko.computed(() => {
