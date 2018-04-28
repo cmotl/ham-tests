06810836b679772c3ba62d1e897e06c9e38eaf5e
Christopher Motl <cmotl@cmotl.com>
2018-04-28 09:54:57 -0500

    :pencil: Allow selection of elements
    Problem:
    - The only questions available are from the Extra element.
    
    Solution:
    - Allow selection of Technician and General in addition to Extra.
    
    Note:
    - Yes, there is duplicate code. Not sure of how I want to get rid of it yet. Sorry Uncle Bob :(

---
+++ app/lib/technician_pool.js
@@ -0,0 +1,31 @@
+const technician = require('hampool/tech')
+
+const map_question = (q) => {
+  return {
+    id: q.id,
+    question: q.question
+  }
+}
+
+const map_answers = (as, correct) => {
+  return as.map((x, i) => map_answer(x, i, correct))
+}
+
+const map_answer = (a, position, correct) => {
+  return {
+    text: a,
+    correct: position == correct
+  }
+}
+
+const pool = () => {
+  return technician.questions.map((q) => {
+    var new_q = map_question(q)
+    new_q.answers = map_answers(q.answers, q.correct)
+    return new_q
+  })
+}
+
+module.exports = {
+  pool: pool
+}
