8ffd19faa6685dcd00279fde20b1fc32eefa2c9a
Christopher Motl <cmotl@cmotl.com>
2019-04-03 17:33:23 -0500

    :arrow_up: Update dependencies for how Heroku uses them.
    Problem:
    - Heroku prunes devDependencies, and several run-time dependencies are in this section.
    - The `content` opening div tag was mistakenly removed in a previous commit.
    
    Solution:
    - Move dependencies to the correct location and add the missing open div tag.

--- interactive.sh
+++ interactive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p .home
-docker run -it --rm -p 3333:3333 -u $(id -u):$(id -g) -e "HOME=/src/.home" -v ${PWD}:/src -w /src node:9.11.1 bash
+docker run -it --rm -p 3333:3333 -u $(id -u):$(id -g) -e "HOME=/src/.home" -v ${PWD}:/src -w /src node:10.15.3 bash
